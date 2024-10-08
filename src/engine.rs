8b74539d847c5b99c7b3f51b4596e3963018bb5a
Tom Crawley <117533212+TheCookiess@users.noreply.github.com>
2024-10-08 20:48:14 +0100

    init commit

--- src/engine.rs
+++ src/engine.rs
@@ -1,3 +1,5 @@
+use winit::event_loop::EventLoop;
+
 const BYTES_PER_PIXEL: usize = 4;
 
 pub trait Renderer {
@@ -27,3 +29,40 @@ pub trait Renderer {
 //     running: bool,
 //     buf_size: (u32, u32),
 // }
+pub struct WindowsRenderer {
+        event_loop: EventLoop<T>;
+}
+
+impl Renderer for WindowsRenderer {
+    fn get_window_size(&self) -> (u32, u32) {
+        todo!()
+    }
+
+    fn get_cursor_pos(&self) -> (u32, u32) {
+        todo!()
+    }
+
+    fn is_mouse_down(&self) -> bool {
+        todo!()
+    }
+
+    fn is_running(&self) -> bool {
+        todo!()
+    }
+
+    fn push_change(&mut self, colour: u32, x: u32, y: u32, scale: u32) {
+        todo!()
+    }
+
+    fn handle_events(&mut self) {
+        todo!()
+    }
+
+    fn render_frame(&mut self) {
+        todo!()
+    }
+
+    fn new(title: &str, width: u32, height: u32) -> Self {
+        todo!()
+    }
+}
